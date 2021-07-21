b8e42d9ecfcc669f64120fd820ac061ea1c41710
katsuyaw <kw@kmail.me>
2021-07-21 12:34:44 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./index.css";
+import Home from "./pages/index";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Router>
+    </>
+  );
 }
 
 export default App;
