4f47aff29f1ef9b5c77d1c2b0ee04ad9f7de167c
katsuyaw <kw@kmail.me>
2021-07-21 11:48:59 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-yellow-300">Hello My Efficiency</div>
+    </>
+  );
+}
+
+export default App;
