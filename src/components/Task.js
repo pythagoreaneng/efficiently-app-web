38a9faafd9e4b33ea335996d3a7150f1684a415e
Nasser <nasserbenyed@gmail.com>
2021-07-24 18:17:23 -0700

    test

--- src/components/Task.js
+++ src/components/Task.js
@@ -18,13 +18,7 @@ const Task = ({ tasks }) => {
     background-color: red;
   `;
 
-  const CompletedButton = styled.button``;
-<<<<<<< HEAD
-
-  return(<div>{tasks}</div>)
-=======
   return <div>{tasks.title}</div>;
->>>>>>> 7eb13cc7e2d8abb061b2bd455950bd6c151a21bf
 };
 
 export default Task;
