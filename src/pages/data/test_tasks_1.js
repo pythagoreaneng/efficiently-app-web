121654497009bdefb522b3ba42196b0ee0305e06
katsuyaw <kw@kmail.me>
2021-07-26 13:06:18 -0700

    Add search

---
+++ src/pages/data/test_tasks_1.js
@@ -0,0 +1,44 @@
+const test_tasks_1 = [
+  {
+    key: 1,
+    title: "sample title 1",
+    completed: true,
+    star: true,
+    "created-date": "07-11-2021",
+    "due-date": "07-11-2021",
+  },
+  {
+    key: 2,
+    title: "sample title 2",
+    completed: false,
+    star: true,
+    "created-date": "07-11-2021",
+    "due-date": "07-11-2021",
+  },
+  {
+    key: 3,
+    title: "sample title 3",
+    completed: false,
+    star: true,
+    "created-date": "07-11-2021",
+    "due-date": "07-11-2021",
+  },
+  {
+    key: 4,
+    title: "sample title 4",
+    completed: false,
+    star: true,
+    "created-date": "07-11-2021",
+    "due-date": "07-11-2021",
+  },
+  {
+    key: 5,
+    title: "sample title 5",
+    completed: true,
+    star: false,
+    "created-date": "07-24-2021",
+    "due-date": "07-24-2021",
+  },
+];
+
+export default test_tasks_1;
