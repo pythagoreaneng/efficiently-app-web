b8495d518ea608b4ecf0c4143338cc6d2e458d2c
katsuyaw <kw@kmail.me>
2021-07-28 15:53:37 -0700

    Create file

---
+++ src/pages/data/test_tasks_2.js
@@ -0,0 +1,49 @@
+var test_tasks_2 = [
+  {
+    key: 1,
+    title: "sample title 1",
+    completed: true,
+    star: true,
+    createdDate: "07-11-2021",
+    dueDate: "07-11-2021",
+    schedule_date: "08/28/2021",
+  },
+  {
+    key: 2,
+    title: "sample title 2",
+    completed: false,
+    star: true,
+    createdDate: "07-11-2021",
+    dueDate: "07-11-2021",
+    schedule_date: "08/28/2021",
+  },
+  {
+    key: 3,
+    title: "sample title 3",
+    completed: false,
+    star: true,
+    createdDate: "07-11-2021",
+    dueDate: "07-11-2021",
+    schedule_date: "01/28/2021",
+  },
+  {
+    key: 4,
+    title: "sample title 4",
+    completed: false,
+    star: true,
+    createdDate: "07-11-2021",
+    dueDate: "07-11-2021",
+    schedule_date: "03/28/2021",
+  },
+  {
+    key: 5,
+    title: "sample title 5",
+    completed: true,
+    star: false,
+    createdDate: "07-24-2021",
+    dueDate: "07-24-2021",
+    schedule_date: "12/28/2021",
+  },
+];
+
+export default test_tasks_2;
