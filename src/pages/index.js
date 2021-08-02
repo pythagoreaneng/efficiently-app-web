b90834d42c884f626b80d13472142a0b387a11f2
katsuyaw <kw@kmail.me>
2021-08-01 20:32:19 -0700

    Add task folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-export { default as Inbox } from "./inbox";
-export { default as Today } from "./today";
-export { default as Star } from "./star";
-export { default as Upcoming } from "./upcoming";
-export { default as Archive } from "./archive";
-export { default as Search } from "./search";
+export { default as Inbox } from "./tasks/inbox";
+export { default as Today } from "./tasks/today";
+export { default as Star } from "./tasks/star";
+export { default as Upcoming } from "./tasks/upcoming";
+export { default as Archive } from "./tasks/archive";
+export { default as Search } from "./tasks/search";
+export { default as Login } from "./login";
