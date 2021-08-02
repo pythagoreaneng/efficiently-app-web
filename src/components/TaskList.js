4dbb7101bd12b403f915f9962cff1f69539efc2b
katsuyaw <kw@kmail.me>
2021-08-02 14:33:42 -0700

    Add renderingTasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,19 +2,8 @@ import React, { useContext } from "react";
 import { TaskContext } from "../contexts/TaskContext";
 import Task from "./Task";
 
-// TaskList displays a filtered list of tasks depending on a folder props passed into it
-// Embeded insided TaskScreen
-
-const TaskList = ({}) => {
-  const {
-    tasks,
-    setTasks,
-    folder,
-    todayDate,
-    completeTask,
-    filteredTasks,
-    pathName,
-  } = useContext(TaskContext);
+const TaskList = () => {
+  const { tasks, todayDate, filteredTasks } = useContext(TaskContext);
 
   const inboxTasks = tasks.filter((task) => !task.completed);
   const archiveTasks = tasks.filter((task) => task.completed);
@@ -22,42 +11,25 @@ const TaskList = ({}) => {
   const starTasks = tasks.filter((task) => task.star);
   const todayTasks = tasks.filter((task) => task.dueDate === todayDate);
 
-  console.log(window.location.pathname);
+  var renderingTasks = tasks; // type of tasks to be rendered
 
-  if (window.location.pathname === "/inbox") {
-    // diplays inbox, which is task.completed = false
-    return inboxTasks.map((task) => {
-      return <Task key={task.id} task={task} />;
-    });
-  } else if (window.location.pathname === "/archive") {
-    return archiveTasks.map((task) => {
-      return <Task key={task.id} task={task} />;
-    });
+  if (window.location.pathname === "/archive") {
+    renderingTasks = archiveTasks;
   } else if (window.location.pathname === "/upcoming") {
-    return upcomingTasks.map((task) => {
-      return <Task key={task.id} task={task} />;
-    });
+    renderingTasks = upcomingTasks;
   } else if (window.location.pathname === "/star") {
-    return starTasks.map((task) => {
-      return <Task key={task.id} task={task} />;
-    });
+    renderingTasks = starTasks;
   } else if (window.location.pathname === "/today") {
-    return todayTasks.map((task) => {
-      return <Task key={task.id} task={task} />;
-    });
+    renderingTasks = todayTasks;
   } else if (window.location.pathname === "/search") {
-    // no filtering
-    return filteredTasks.map((task) => {
-      return <Task key={task.id} task={task} />;
-    });
+    renderingTasks = filteredTasks;
   } else {
-    // any other cases displays inbox
-    return filteredTasks
-      .filter((task) => !task.completed)
-      .map((task) => {
-        return <Task key={task.id} task={task} />;
-      });
+    // render inbox for any other cases
+    renderingTasks = inboxTasks;
   }
+  return renderingTasks.map((task) => {
+    return <Task key={task.id} task={task} />;
+  });
 };
 
 export default TaskList;
