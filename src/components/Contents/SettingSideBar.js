24d2bb59912e280c8cb6a77c4d4aed05d13565c4
katsuyawakabayashi <kw@kmail.me>
2021-09-03 12:21:33 -0700

    Remove upgrade

--- src/components/Contents/SettingSideBar.js
+++ src/components/Contents/SettingSideBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { CgProfile } from "react-icons/cg";
-import { BiBoltCircle } from "react-icons/bi";
 import { AiOutlineFormatPainter, AiOutlineLock } from "react-icons/ai";
 import { NavCard } from "../Body";
 
@@ -10,7 +9,6 @@ const SettingSideBar = () => {
       <NavCard icon={<CgProfile />} type={"profile"} />
       <NavCard icon={<AiOutlineFormatPainter />} type={"customize"} />
       <NavCard icon={<AiOutlineLock />} type={"security"} />
-      <NavCard icon={<BiBoltCircle />} type={"upgrade"} />
     </>
   );
 };
